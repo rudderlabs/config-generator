6a47536c6db94650f4447aec12eea16f48d033a5
lokey <lokeshiitm08@gmail.com>
2021-06-02 06:42:00 +0530

    Add endpoint for loading and saving config to the backend

--- server/index.js
+++ server/index.js
@@ -11,14 +11,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-
 
 // app.use(basicAuth({
 //     users: { [adminUser]: adminPassword }
 // }))
+
 app.use(express.json())
 
 
@@ -26,6 +23,53 @@ app.post('/saveToFile', function (req, res) {
   const configSavePath = process.env.REACT_APP_SAVE_TO_FILE_PATH || './config.json';
   fs.writeFileSync(configSavePath, JSON.stringify(req.body.workspaceConfig));
   res.send('Saved to file');
+});
+
+const loadJSONFile = (path) => {
+  if (fs.existsSync(path)) {
+    return JSON.parse(fs.readFileSync(path, 'utf8'));
+  } else {
+    return "{}";
+  }
+}
+
+app.get('/loadConnections', function(req, res) {
+  connections = loadJSONFile('./connections.json');
+  res.send({connections});
+});
+
+app.get('/loadSources', function(req, res) {
+  sources = loadJSONFile('./sources.json');
+  res.send({sources});
+});
+
+app.get('/loadDestinations', function(req, res) {
+  destinations = loadJSONFile('./destinations.json');
+  res.send({destinations});
+});
+
+
+app.post('/saveConnections', function (req, res) {
+  const connectionsSavePath = './connections.json';
+  fs.writeFileSync(connectionsSavePath, JSON.stringify(req.body.connections));
+  res.send('Saved connections to file');
+});
+
+app.post('/saveDestinations', function (req, res) {
+  const destinationsSavePath = './destinations.json';
+  fs.writeFileSync(destinationsSavePath, JSON.stringify(req.body.destinations));
+  res.send('Saved destinations to file');
+});
+
+app.post('/saveSources', function (req, res) {
+  const sourcesSavePath = './sources.json';
+  fs.writeFileSync(sourcesSavePath, JSON.stringify(req.body.sources));
+  res.send('Saved sources to file');
+});
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(9000);
